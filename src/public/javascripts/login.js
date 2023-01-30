ddcaabc340a0f66f51682cdaf15421713bb057d7
gani0325 <lge0325@bme.gachon.ac.kr>
2023-01-30 23:40:14 +0900

    [FEAT] 캘린더 div에 삽입 완료

---
+++ src/public/javascripts/login.js
@@ -0,0 +1,11 @@
+//@ 로그인 페이지에서 작동하는 JS
+//@ Authentication strategies (인증전략)
+//@ Application middleware (미들웨어)
+'use strict';
+
+const passport = require('passport');
+
+router.post('/login', );
+
+passport.authenticate('')
+
