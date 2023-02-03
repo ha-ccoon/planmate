a5c0a02c9f23240352e91b88522d08f1671ac404
minha sohn <codeminhasohn@gmail.com>
2023-02-03 10:44:43 +0900

    [ADD] register 화면 POST 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,33 @@
 'use strict';
 const express = require('express');
+const { response } = require('../../app');
 const router = express.Router();
+const { User } = require('../models');
 
 //@ localhost:3000
 router.get('/', (req, res) => {
   res.render('index');
 });
 
+router.post('/', async (req, res) => {});
+
 //@ localhost:3000/register
 router.get('/register', (req, res) => {
   res.render('register');
+});
+
+router.post('/register', async (req, res) => {
+  const { email, password, firstName, lastName, phoneNumber, birthDay } =
+    req.body;
+  const userInfo = await User.create({
+    email,
+    password,
+    firstName,
+    lastName,
+    phoneNumber,
+    birthDay,
+  });
+  res.json(userInfo);
 });
 
 //@ localhost:3000/main
