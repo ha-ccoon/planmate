148bbc5924f97174b925612f50bcf2d55f9d901b
minha sohn <codeminhasohn@gmail.com>
2023-02-08 05:06:58 +0900

    [CHORE] 로그인 기능 구현중

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,57 @@
 'use strict';
+const passport = require('passport');
 const express = require('express');
-const { response } = require('../../app');
 const router = express.Router();
-const { User } = require('../models');
-const crypto = require('crypto');
 const bcrypt = require('bcrypt');
-const { userInfo } = require('os');
+
+const { User } = require('../models');
+const { isLoggedIn, isNotLoggedIn } = require('./passport/logInStatus');
+const passportPublic = require('../public/javascripts/passport/index');
 
 //@ localhost:3000
 router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.post('/', async (req, res) => {});
+router.post('/', isNotLoggedIn, (req, res, next) => {
+  // localStrategy 호출
+  passport.authenticate('local', (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      console.log(authError);
+      alert('가입되지 않은 회원입니다.');
+      return next(authError); // 에러처리 미들웨어로 실행
+    }
+    if (!user) {
+      console.log('password is not correct');
+      return alert('비밀번호가 일치하지 않습니다');
+    }
+    // 로그인 성공. index 호출
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect('/main');
+      console.log('login success');
+    });
+  })(req, res, next);
+});
 
 //@ localhost:3000/register
 router.get('/register', (req, res) => {
   res.render('register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', isNotLoggedIn, async (req, res) => {
   try {
     const { email, password, firstName, lastName, phoneNumber, birthDay } =
       req.body;
+    // 기존 유저 확인 (중복 가입 방지)
+    const existedUser = await User.findOne({ where: { email } });
+    if (existedUser) {
+      return res.redirect('/');
+    }
     const hash = await bcrypt.hash(password, 12);
     const userInfo = await User.create({
       email,
@@ -33,7 +62,7 @@ router.post('/register', async (req, res) => {
       birthDay,
     });
 
-    // userInfo.save();
+    userInfo.save();
     console.log('your account is registered');
     res.redirect('/');
   } catch (error) {
