9477a580c86da86922c9e9cbd92beee9533b85d7
김정연 <jy.keem2468@gmail.com>
2023-02-09 23:16:05 +0900

    [ADD]schemas, routes에 plan부분 변수선언

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,12 @@
 //@ 스키마 모델화 JS
-const mongoose = require('mongoose');
-const UserSchema = require('./Schemas/User');
-const CalendarSchema = require('./Schemas/Calendar');
-const DiarySchema = require('./Schemas/Diarys')
-
-
+const mongoose = require("mongoose");
+const UserSchema = require("./Schemas/User");
+const CalendarSchema = require("./Schemas/Calendar");
+const DiarySchema = require("./Schemas/Diarys");
+const PlanSchema = require("./Schemas/Plans");
 
 // 참조 (모델 이름, 스키마, Collections 이름)
-exports.User = mongoose.model('User', UserSchema, 'UserList');
-exports.Calendar = mongoose.model('Calendar', CalendarSchema, 'CalendarList');
-exports.Diary = mongoose.model('Diary', DiarySchema, 'DiaryList');
+exports.User = mongoose.model("User", UserSchema, "UserList");
+exports.Calendar = mongoose.model("Calendar", CalendarSchema, "CalendarList");
+exports.Diary = mongoose.model("Diary", DiarySchema, "DiaryList");
+exports.Plan = mongoose.model("Plan", PlanSchema, "PlanList");
